24c193e6745fdf4f1744974cb7e0c697bc28a003
David Goh <dg282@duke.edu>
2025-08-04 11:35:39 +0800

    created assignment 2 in python

---
+++ module_2_assignment/countInv.py
@@ -0,0 +1,11 @@
+# goal: compute the number of inversions in the file given, where the ith row of the file indicates the ith entry of an array.
+# input: some txt file containing n integers (the final one is 100,000)
+# output: an int
+# test case: make a txt file with 1,3,5,2,4,6
+
+import math 
+
+def countInv(x: int[]) -> int:
+    count = 0
+
+    return count
