b637ab48ecbc3b4520b2767b3e74bfba319689af
David Goh <dg282@duke.edu>
2025-08-06 12:31:16 +0800

    taking inspiration from mrunalnshah, built and tested build_graph

--- course_2/module_1_assignment/kosaraju.py
+++ course_2/module_1_assignment/kosaraju.py
@@ -6,40 +6,57 @@
 # run DFS-loop on the graph again but start from the largest finishing times
 
 import sys
+from collections import defaultdict
+
 
 def parse_graph(filepath):
     with open(filepath, 'r') as f:
         edges = [int(line.strip()) for line in f if line.strip()]    
     return edges
 
+edges = [
+    (1, 2), (2, 3), (3, 1), (3, 4), (4, 5)
+]
+num_nodes = 5
+
+def build_graph(edges, num_nodes):
+    G = defaultdict(list)
+    G_rev = defaultdict(list)
+    for tail, head in edges:
+        G[tail].append(head)
+        G_rev[head].append(tail)
+    # Ensure all nodes exist in both graphs
+    for node in range(1, num_nodes + 1):
+        G[node]
+        G_rev[node]
+    return G, G_rev
+
+print(build_graph(edges, num_nodes))    
+
+
 def first_pass_loop(G_rev, nodes):
     explored = set()
     finishing_times = {}
+    finishing_order = []
     t = [0]
     for node in sorted(nodes, reverse=True):
         if node not in explored:
-            dfs_first_pass(G_rev, node, explored, finishing_times, t)
-    return finishing_times
+            dfs_first_pass(G_rev, node, explored, finishing_times, t, finishing_order)
+    return finishing_times, finishing_order
 
-def dfs_first_pass(G_rev, node, explored, finishing_times, t):
+def dfs_first_pass(G_rev, node, explored, finishing_times, t, finishing_order):
     explored.add(node)
-    for nei in G_rev.get(node, []):
+    for nei in sorted(G_rev.get(node, []), reverse=True):
         if nei not in explored:
-            dfs_first_pass(G_rev, nei, explored, finishing_times, t)
+            dfs_first_pass(G_rev, nei, explored, finishing_times, t, finishing_order)
     t[0] += 1
     finishing_times[node] = t[0]
+    finishing_order.append(node)
 
-G_rev = {
-    2: [1],
-    3: [2],
-    1: [3],
-    4: [3],
-    5: [4],
-}
-nodes = {1,2,3,4,5}
 
-ftimes = first_pass_loop(G_rev, nodes)
-print(ftimes)
+# ftimes, order = first_pass_loop(G_rev, nodes)
+# print("Finishing times:", ftimes)
+# print("Finishing order:", order)
 
 # basically starting from node n, you DFS it and label finishing times on the way out. 
 # i think after the above is done i would want to check with toy examples if the labelling of finishing times works properly
@@ -53,5 +70,7 @@ print(ftimes)
 #         sys.exit(1)
 #     filepath = sys.argv[1]
 #     edges = parse_graph(filepath)
-#     sizes = main(edges)
+#     G, G_rev = build_graph(edges)    
+# sizes = main()
 #     print(sizes)
+
