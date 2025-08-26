300b1ac7a4aed0abc1a19647018903d1ecf96a3b
David Goh <dg282@duke.edu>
2025-08-26 11:38:07 -0400

    ok done module 3

--- course_3/module_3/huffman.py
+++ course_3/module_3/huffman.py
@@ -6,13 +6,13 @@
 # pseudo code should be something like 
 
 ## parse data
-# parse the file by adding all of them to a heap basically
 # initialize a heap
+# parse the file by adding all of them to a heap basically
 
 ## build tree
-# initialize tree structure with pointers?
-# do extract min twice, get a and b
-# make a node point to a (left) and b (right) # merge tree step
+# initialize tree structure with pointers? basically i think we need to start with all the nodes separate and merge as we go along
+# do extract min on heap twice, get a and b
+# in tree, make a node point to a (left) and b (right) # merge tree step
 # heap-- add a+b
 # repeat until done
 
@@ -24,3 +24,43 @@
 
 ##main function basically calls the three functions above in sequence, but 
 # do traverse tree search twice for lowest and highest frequency character
+
+import heapq
+
+def huffman_code_lengths_from_file(path):
+    # parse the weights
+    with open(path) as f:
+        n = int(f.readline().strip())        # number of symbols
+        weights = [int(line.strip()) for line in f]
+
+    #initialize the heap
+    # Each heap element is a tuple: (weight, min_depth, max_depth).
+    # Leaf nodes start with depth = 0 (since no edges yet).
+    heap = [(w, 0, 0) for w in weights]
+    heapq.heapify(heap)
+
+    # do the tree buiding process
+    while len(heap) > 1:
+        a = heapq.heappop(heap)
+        b = heapq.heappop(heap)
+        # Merge two nodes: weight adds up.
+        # Depth increases by 1 for all leaves under this new node.
+        new_weight = a[0] + b[0]
+        new_min = min(a[1], b[1]) + 1
+        new_max = max(a[2], b[2]) + 1
+        heapq.heappush(heap, (new_weight, new_min, new_max))
+
+    _, min_len, max_len = heap[0]
+    return min_len, max_len
+
+# Example run
+import sys 
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python huffman.py <inputfile>")
+        sys.exit(1)
+    infile = sys.argv[1]
+    min_len, max_len = huffman_code_lengths_from_file(infile)
+    print("Minimum codeword length:", min_len)
+    print("Maximum codeword length:", max_len)
--- course_3/module_3/mwis.py
+++ course_3/module_3/mwis.py
@@ -16,3 +16,42 @@
 # return string in proper order :)
 
 # i chose to do this all together instead of modularising because the solutions array should be global enough for the reconstruction to access
+
+def mwis_membership(path):
+    n = len(path) - 1  # path[1..n], 0 is dummy
+    A = [0] * (n + 1)   # initialize an array to all zeroes which is the length of the path. we're going to build this array left to right
+    A[1] = path[1]
+
+    for i in range(2, n + 1):
+        A[i] = max(A[i-1], A[i-2] + path[i])    # in this case path[i] is the weight of the node given in dataset
+
+    # reconstruction
+    chosen = set()
+    i = n
+    while i >= 1:
+        if i == 1:
+            if A[1] > 0: chosen.add(1) # if you reach 1 then you know you added the first object
+            i -= 1
+        elif A[i-1] >= A[i-2] + path[i]: # this is the main step: skip if not bigger
+            i -= 1
+        else:
+            chosen.add(i) 
+            i -= 2
+
+    return chosen
+
+def mwis_from_file(filename):
+        # basically parse weights
+    with open(filename) as f:
+        n = int(f.readline().strip())
+        weights = [0] + [int(line.strip()) for line in f]  # 1-indexed
+
+    chosen = mwis_membership(weights)
+
+    query_vertices = [1, 2, 3, 4, 17, 117, 517, 997]
+    bitstring = ''.join('1' if v in chosen else '0' for v in query_vertices) # this is very useful I must learn
+    return bitstring
+
+if __name__ == "__main__":
+    filename = "mwis.txt"
+    print(mwis_from_file(filename))
